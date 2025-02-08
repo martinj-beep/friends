befeced2ca7623b578fee9a7b21a8522bb98e899
Martynas Jakutis <jakumart@gmail.com>
2025-02-08 19:21:49 +0200

    friends project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :friends
+  # get "home/index"
+  get "home/about"
+  root "home#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
